c882d69535b4de20af3521546dfb2b88f0f58c56
Omar Bautista <bautistaramirezomar0@gmail.com>
2024-09-04 21:31:36 -0600

    Programa listo con el menu

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,68 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
+/*
+* Integrantes:
+* Omar de Jesus Bautista Ramirez
+* Angel Gustavo Lopez Lopez
+* Emmanuel Lopez Villatoro
+* */
+
 
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        char resp;
+        ArrayList<Persona> personas = new ArrayList<>();
+        int menu;
+        do {
+            System.out.println("---Menu---");
+            System.out.println(" 1.- Añadir\n 2.- Eliminar\n 3.- Modificar\n 4.- Consulta\n 5.- Informe\n 6.- Terminar ");
+            menu = sc.nextInt();
+            if (menu==1){
+                //metodo Array para listas dinamicas
+                do {
+                    System.out.println("Ingresar el nombre: ");
+                    String nombre = sc.next();
+                    System.out.println("Ingresar el edad: ");
+                    int edad = sc.nextInt();
+                    System.out.println("Ingresar el peso: ");
+                    int pesos = sc.nextInt();
+                    System.out.println("Ingresar el sexo: ");
+                    char sexo = sc.next().charAt(0);
+                    personas.add( new Persona(nombre, edad, pesos, sexo));
 
-        System.out.println("Cuantos campos desea llenar?");
-        int T = sc.nextInt();
-        Persona[] persona = new Persona[T];
+                    System.out.println("Deseas agregar otra persona (si, no)");
+                    resp = sc.next().charAt(0);
+                }while (resp =='s' || resp=='S');
+            }if (menu==2){
+                System.out.println("Que elemento desea eliminar?");
 
-        for (int i = 0; i < T; i++) {
-            System.out.println("Ingresar el nombre: ");
-            String nombre = sc.next();
-            System.out.println("Ingresar el edad: ");
-            int edad = sc.nextInt();
-            System.out.println("Ingresar el peso: ");
-            int pesos = sc.nextInt();
-            System.out.println("Ingresar el sexo: ");
-            char sexo = sc.next().charAt(0);
-            persona[i] = new Persona(nombre, edad, pesos, sexo);
-        }
+            }if (menu==3){
 
-        for (int i = 0; i < T; i++) {
-            System.out.println(persona[i].getNombre());
-            System.out.println(persona[i].getEdad());
-            System.out.println(persona[i].getPeso());
-            System.out.println(persona[i].getSexo());
+                System.out.println("Que posicion desea modificar?");
+                int P = sc.nextInt();
 
-        }
+            }if (menu==4){
+                System.out.println("Que persona desea consultar?");
+
+
+
+            }if (menu==5){
+
+                //for each que recorra las posiciones
+                for(Persona persona : personas) {
+                    System.out.print(persona.getNombre()+" ");
+                    System.out.print(persona.getEdad()+" ");
+                    System.out.print(persona.getPeso()+" ");
+                    System.out.print(persona.getSexo()+"\n");
+                }
+            }if (menu==6){
+                break;
+            }
+            System.out.println("");
+        }while (menu!=6);
+
+
     }
 }
